a2f9db53013aa5fe4475ce0cdf67c219b6d0715a
LL <li.lin@huairuo.ai>
2020-08-22 18:00:35 +0800

    add file.

---
+++ lib/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Huairuo.ai.
+# Author: Lin Li (li.lin@huairuo.ai)
+#
+# cython: language_level=3
+#
+
+class Config(object):
+  def __init__(self):
+    pass
+
+  def load_from_json(self, json_path):
+    """"""
